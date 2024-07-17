0e315e67144fc2b461995a1388d894fc08fb07e3
pushkalkishore <kpushkal122@gmail.com>
2024-07-17 20:53:41 +0530

    add cors fix for the backend

--- blog-backend/routes/Auth.js
+++ blog-backend/routes/Auth.js
@@ -4,7 +4,14 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const router = express.Router();
+const cors = require("cors");
 require("dotenv").config();
+
+// Configure CORS
+const corsOptions = {
+  origin: "http://localhost:3000", // Allow requests from your frontend domain
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // Signup route
 router.post("/signup", (req, res) => {
