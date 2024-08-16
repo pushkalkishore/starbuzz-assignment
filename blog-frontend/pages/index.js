d25b001ab7624baccf960ea41b309f43e926e548
pushkalkishore <kpushkal122@gmail.com>
2024-08-16 18:43:12 +0530

    this is test commit

--- blog-frontend/pages/index.js
+++ blog-frontend/pages/index.js
@@ -15,8 +15,9 @@ export default function Home() {
       <main className="main">
         <h2>Welcome to Starbuzz.ai Assignment</h2>
         <p>
-          This is a sample project demonstrating a blog application with login,
-          signup, and posts functionality.
+          this is a test line that i am adding This is a sample project
+          demonstrating a blog application with login, signup, and posts
+          functionality.
         </p>
         <p>Use the navigation links above to login, signup, or view posts.</p>
       </main>
